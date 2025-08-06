e0eae6ff8c5483078fbd8e27efc070fb51a49aeb
PaulinaCzarnota <paul.czar.14@gmail.com>
2025-08-06 23:41:54 +0100

    Added WorkoutType enumeration to define available workout types

---
+++ app/src/main/java/com/example/fitnesstrackerapp/data/entity/WorkoutType.kt
@@ -0,0 +1,20 @@
+/**
+ * Workout Type Enumeration
+ *
+ * Defines the available workout types in the fitness tracker
+ */
+package com.example.fitnesstrackerapp.data.entity
+
+/**
+ * Enumeration of supported workout types
+ */
+enum class WorkoutType {
+    RUNNING,
+    CYCLING,
+    WEIGHTLIFTING,
+    WALKING,
+    SWIMMING,
+    YOGA,
+    CARDIO,
+    OTHER
+}
