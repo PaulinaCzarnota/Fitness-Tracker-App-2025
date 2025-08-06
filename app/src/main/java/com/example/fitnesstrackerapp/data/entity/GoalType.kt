13b8bc602b62d123d929695ff0bae2e1a1990c4e
PaulinaCzarnota <paul.czar.14@gmail.com>
2025-08-07 00:36:56 +0100

    Added GoalType enumeration for fitness tracking goal types

---
+++ app/src/main/java/com/example/fitnesstrackerapp/data/entity/GoalType.kt
@@ -0,0 +1,21 @@
+/**
+ * Goal Type Enumeration
+ *
+ * Defines the available goal types for fitness tracking
+ */
+package com.example.fitnesstrackerapp.data.entity
+
+/**
+ * Enumeration of supported goal types
+ */
+enum class GoalType {
+    WEIGHT_LOSS,
+    WEIGHT_GAIN,
+    DISTANCE_RUNNING,
+    WORKOUT_FREQUENCY,
+    CALORIE_BURN,
+    STEP_COUNT,
+    DURATION_EXERCISE,
+    STRENGTH_TRAINING,
+    OTHER
+}
