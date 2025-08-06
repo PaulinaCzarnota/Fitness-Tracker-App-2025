ef1a1050fd745e46ed6310f910061a94bba9a8b0
PaulinaCzarnota <paul.czar.14@gmail.com>
2025-08-06 22:43:25 +0100

    Added WeeklyStats data class for aggregated weekly fitness data

---
+++ app/src/main/java/com/example/fitnesstrackerapp/data/entity/WeeklyStats.kt
@@ -0,0 +1,21 @@
+/**
+ * Weekly Statistics Data Class
+ *
+ * Represents aggregated weekly fitness data
+ */
+package com.example.fitnesstrackerapp.data.entity
+
+/**
+ * Data class representing weekly workout statistics
+ *
+ * @property workoutCount Total number of workouts in the week
+ * @property totalDuration Total workout duration in minutes
+ * @property totalCalories Total calories burned during workouts
+ * @property totalDistance Total distance covered in kilometers
+ */
+data class WeeklyStats(
+    val workoutCount: Int = 0,
+    val totalDuration: Int = 0,
+    val totalCalories: Int = 0,
+    val totalDistance: Float = 0f
+)
